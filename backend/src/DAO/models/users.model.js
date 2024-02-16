74ffa0e906decec422f1b03dfe270cb4a3a3aa88
pablinhoocl <xpabliito@gmail.com>
2024-02-15 22:20:53 -0300

    agrege una base al serverjs y una base del model de usuario igual cree los archivos de los otros modelos

--- backend/src/DAO/models/users.model.js
+++ backend/src/DAO/models/users.model.js
@@ -1,1 +1,48 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const passwordValidator = (value) => {
+  // Check for at least one uppercase letter
+  if (!/[A-Z]/.test(value)) {
+    throw new Error('Password must contain at least one uppercase letter');
+  }
+
+  // Check for at least one symbol (non-alphanumeric character)
+  if (!/[^A-Za-z0-9]/.test(value)) {
+    throw new Error('Password must contain at least one symbol');
+  }
+
+  return true;
+};
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    minlength: 6,
+    required: true,
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: [passwordValidator, 'Password validation failed, It s need at least a mayus character and a symbol'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator(value) {
+        // Expresión regular para validar un correo electrónico
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      },
+      message: (props) => `${props.value} no es un correo electrónico válido`,
+    },
+  },
+  projets: [{ type: Schema.Types.ObjectId, ref: 'Proyect' }],
+});
+
+module.exports = mongoose.model('User', UserSchema);
 // aca defino el modelo de mongo del usuario
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,55 @@
-import { Express } from "express";
+import { express } from 'express';
+import { path } from 'path';
+import { session } from 'express-session';
+import { mongoose } from 'mongoose';
+import { dotenv } from 'dotenv';
+import { cors } from 'cors';
+import { morgan } from 'morgan';
+
+dotenv.config();
+
+mongoose.set('strictQuery', false);
+
+const { mongoUrl } = process.env;
+
+const database = 'postBoard';
+
+const mongoDb = `${mongoUrl + database}?retryWrites=true&w=majority`;
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(mongoDb, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1);
+  }
+}
+
+connectToDatabase();
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(session({
+  secret: 'mysecret',
+  cookie: { maxAge: 3600000 },
+  saveUninitialized: false,
+  resave: false,
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('dev'));
 
 // arhivo base del proyecto. Aca
 // se encuentra toda la config del proyecto
 // como router, config de auths , etc
-
